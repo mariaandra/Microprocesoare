eb1e9bc5667d296a90190f3e15298d9adc5eb0a2
OanaRevnic <115151640+OanaRevnic@users.noreply.github.com>
2022-12-15 12:46:54 +0200

    Add files via upload
    Cod final

--- GraphBuzzerVibration/final.py
+++ GraphBuzzerVibration/final.py
@@ -9,24 +9,27 @@ def main():
     counter = len(num_list)
     max_val = max(num_list)
     min_val = min(num_list)
-    med_val = (max_val + min_val) / 2;
+    med_val = (max_val + min_val) / 2
 
     x = np.arange(counter)
     y = num_list
 
     vector_sec = np.vectorize(np.float_)
-    vector_hz = np.vectorize(np.float_)
+    vector_v = np.vectorize(np.float_)
 
     sec = vector_sec(x)
-    hz = vector_hz(y)
+    v = vector_v(y)
 
     # am luat range-ul pentru valoare medie ca fiind [med_val - 0.7, med_val + 0.7]
-    upper = med_val + 0.7
-    lower = med_val - 0.7
+    print(med_val)
+    upper = med_val + 0.4
+    lower = med_val - 1.4
+    print(upper)
+    print(lower)
 
-    supper = np.ma.masked_where(hz < upper, hz)  # prag mare -> verde
-    slower = np.ma.masked_where(hz > lower, hz)  # prag mic -> portocaliu
-    smiddle = np.ma.masked_where((hz < lower) | (hz > upper), hz)  # prag mediu -> albastru
+    supper = np.ma.masked_where(v < upper, v)  # prag mare -> verde
+    slower = np.ma.masked_where(v > lower, v)  # prag mic -> portocaliu
+    smiddle = np.ma.masked_where((v < lower) | (v > upper), v)  # prag mediu -> albastru
 
     fig, ax = plt.subplots()
     ax.plot(sec, smiddle, sec, slower, sec, supper)
--- GraphBuzzerVibration/main.py
+++ GraphBuzzerVibration/main.py
@@ -1,16 +1,7 @@
-from random import randint
-import numpy as np
-import random
 import PyQt5.QtWidgets as QtWidgets
-import matplotlib.pyplot as plt
-from pyqtgraph import PlotWidget, plot
 import pyqtgraph as pg
 import sys
-import os
 import serial
-import re
-import time
-
 from pyqtgraph.Qt import QtCore
 
 
@@ -23,17 +14,17 @@ class MainWindow(QtWidgets.QMainWindow):
         self.graphWidget = pg.PlotWidget()
         self.setCentralWidget(self.graphWidget)
         self.graphWidget.setBackground((245, 245, 245))
-        self.graphWidget.setTitle("\nGetting values\n", color="black", size="18px")
+        self.graphWidget.setTitle("\nGetting values\n", color="black", size="18px123")
 
         styles = {'color': 'black', 'font-size': '12px'}
-        self.graphWidget.setLabel('left', 'Frequency (Hz)', **styles)
+        self.graphWidget.setLabel('left', 'Frequency (V)', **styles)
         self.graphWidget.setLabel('bottom', 'Time (s)', **styles)
         self.graphWidget.showGrid(x=True, y=True)
         self.graphWidget.setYRange(0, 3.3, padding=0)  # range la Hz (0,3.3)
 
         # frecventa si secundele ar veni aici
-        self.x = list(range(100))  # 100 time points
-        self.y = [random.uniform(0, 100) for _ in range(100)]  # 100 data points
+        self.x = list(range(10))
+        self.y = [0] * 10
 
         # pen
         pen = pg.mkPen(color=(0, 204, 0), width=2)
@@ -46,28 +37,32 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # timer
         self.timer = QtCore.QTimer()
-        self.timer.setInterval(5)
+        self.timer.setInterval(30)
         self.timer.timeout.connect(self.update_plot_data)
         self.timer.start()
 
-    def update_plot_data(self):
-        while True:
-            if self.serialPort.in_waiting > 0:
-                f = open("values.txt", "a")
-                serialString = self.serialPort.readline()
-                linedec = serialString.decode("Ascii")
-                linefloat = float(linedec)
-                self.x = self.x[1:]
-                self.x.append(self.x[-1] + 1)
 
-                self.y = self.y[1:]  # Remove the first
-                self.y.append(linefloat)
-                f.write(f"{linefloat}\n")
-                print(linefloat)
-                self.data_line.setData(self.x, self.y)  # Update
+    def update_plot_data(self):
+        if self.serialPort.in_waiting > 0:
+            f = open("values.txt", "a")
+            serialString = self.serialPort.readline()
+            linedec = serialString.decode("Ascii")
+            linefloat = float(linedec)
+
+            self.x = self.x[1:]  # Remove the first y element.
+            self.x.append(self.x[-1] + 1)  # Add a new value 1 higher than the last.
+
+            self.y = self.y[1:]  # Remove the first
+            self.y.append(linefloat)  # Add a new random value.
+
+            f.write(f"{linefloat}\n")
+            print(linefloat)
+            self.data_line.setData(self.x, self.y)  # Update
 
 
 def main():
+    with open('values.txt', 'w'):
+        pass
     app = QtWidgets.QApplication(sys.argv)
     main = MainWindow()
     main.show()
